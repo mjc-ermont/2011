3e6360bf038eea0e270fe287dd18eab6db8c7427
konfiot <arthurtoussaint@free.fr>
2012-10-27 17:03:33 +0200

    Tweaks pour faire marcher le capteur de pression

--- Programmation/Fragments de code/Pression/AnalogInput/AnalogInput.ino
+++ Programmation/Fragments de code/Pression/AnalogInput/AnalogInput.ino
@@ -1,18 +1,21 @@
 void setup() {
+  
   Serial.begin(9600); 
 }
  
 void loop() {
-  int sensorValue = analogRead(15);
-    Serial.println(sensorValue);                                                        
-  float volt = ((float)sensorValue*5)/1023;
+   int sensorValue;
+   
+   sensorValue = analogRead(5);  
+   float volt = ((float)sensorValue*5)/1023;
+   Serial.println(volt);  
+   
+   float pr = ((volt-0.2)/(0.045));
+   
+   //int alt = -99.01*pow(sensorValue,5) + 1135*pow(sensorValue,4) - 4843.2*pow(sensorValue,3) + 9980.9*pow(sensorValue,2) - 13661*sensorValue + 15781;
+   
+   Serial.println(pr);
+   //Serial.println(alt);  
   
-  float pr = ((volt)/(0.045))-0.2;
-  
-  int alt = -99.01*pow(sensorValue,5) + 1135*pow(sensorValue,4) - 4843.2*pow(sensorValue,3) + 9980.9*pow(sensorValue,2) - 13661*sensorValue + 15781;
-  
-  Serial.println(pr);
-  //Serial.println(alt);  
-  
-  delay(500);
+   delay(500);
 }
