cf536731dacc15f7611d79b4b035fbfdf0c92d1f
konfiot <arthurtoussaint@free.fr>
2012-12-25 15:55:01 +0100

    Commentage des defines

--- Programmation/Codes fonctionnels/Arduino/defines.h
+++ Programmation/Codes fonctionnels/Arduino/defines.h
@@ -1,24 +1,28 @@
 /*  ------- DEBUG -------  */
 
-#define SERIAL_DEBUG    1
-#define SERIAL_BAUDRATE 4800
+#define SERIAL_DEBUG    1    // Activation du mode debug
 
 /*  ------- TRASMISSION -------  */
 
-#define NB_REPET        3
-#define SIZE_VALUE      7
-#define GPS_BAUDRATE    4800
-#define ID_CAPT_GPS     "00"
-#define ID_CAPT_ACCEL   "01"
-#define ID_CAPT_HUM     "02"
-#define ID_CAPT_PRESS   "03"
-#define ID_CAPT_TEMP    "04"
-#define NB_VAL_GPS      7
-#define ID_VAL_LAT_DEG  "0"
-#define ID_VAL_LAT_MIN  "1"
-#define ID_VAL_LON_DEG  "2"
-#define ID_VAL_LON_MIN  "3"
-#define ID_VAL_VIT      "4"
-#define ID_VAL_UTIME    "5"
+#define SERIAL_BAUDRATE 4800 // baudrate de la liaison série avec le kiwi
+#define NB_REPET        3    // Nombre de répéition de la trame
+#define SIZE_VALUE      7    // Longueur maxi de la valeur dans la trame
+#define ID_CAPT_GPS     "00" // ID dans la trame du GPS
+#define ID_CAPT_ACCEL   "01" // ID dans la trame de l'accéléromètre
+#define ID_CAPT_HUM     "02" // ID dans la trame du capeur d'humidité
+#define ID_CAPT_PRESS   "03" // ID dans la trame du capeur de pression
+#define ID_CAPT_TEMP    "04" // ID dans la trame du capeur de temperature
+// GPS
+#define GPS_BAUDRATE    4800 // baudrate de la liaison série avec le GPS
+#define NB_VAL_GPS      7    // Nombre de valeurs du capteur GPS
+#define ID_VAL_LAT_DEG  "0"  // No de la valeur "Degres latitude" dans la trame
+#define ID_VAL_LAT_MIN  "1"  // No de la valeur "Minutes latitude" dans la trame
+#define ID_VAL_LON_DEG  "2"  // No de la valeur "Degres longitude" dans la trame
+#define ID_VAL_LON_MIN  "3"  // No de la valeur "Minutes longitude" dans la trame
+#define ID_VAL_VIT      "4"  // No de la valeur "Vitesse" dans la trame
+#define ID_VAL_UTIME    "5"  // No de la valeur "temps UTC" dans la trame
 
-#define HYT371_ADDR     0x28
+
+/*  ------- CAPTEUR HMIDITE -------  */
+
+#define HYT371_ADDR     0x28 // Adresse I2C Capteur Humidité
